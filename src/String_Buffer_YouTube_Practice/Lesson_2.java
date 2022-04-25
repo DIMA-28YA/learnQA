5fc64898f66e73557edb2cc0809d8420b55a9889
DIMA-28YA <28yan4a87@gmail.com>
2022-04-25 12:09:30 +0300

    Three commit message

--- src/String_Buffer_YouTube_Practice/Lesson_2.java
+++ src/String_Buffer_YouTube_Practice/Lesson_2.java
@@ -1,6 +1,19 @@
 package String_Buffer_YouTube_Practice;
 
+import java.util.Scanner;
+
 public class Lesson_2 {
+    public static void main(String[] args) {
+        // сравнить имена двух людей
+        String name1 = new Scanner(System.in).nextLine();
+        String name2 = new Scanner(System.in).nextLine();
+if (name1.equals(name2)) {
+    System.out.println("Равны");
+}else {
+    System.out.println("Не равны");
+}
 
 
+
+    }
 }
